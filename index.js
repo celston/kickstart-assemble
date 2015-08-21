a3a04d58a5545c86ce9a1ce8a6f386e8f5be52a0
askesian <cwigley@gmail.com>
2015-08-21 13:47:30 -0500

    Cleanup default and user options handling

--- index.js
+++ index.js
@@ -30,10 +30,12 @@ module.exports.styleguide = compileStyleguide;
  *
  * @return {[type]}
  */
-function compileTemplates(options, done) {
+function compileTemplates(opts, done) {
   var app = assemble.init();
-  var opts = options || {};
   var cwd = process.cwd();
+
+  opts = opts || {};
+
   var isProduction = opts.production || false;
   var assets = opts.assets || '';
   var helpers = opts.helpers || [];
@@ -44,11 +46,12 @@ function compileTemplates(options, done) {
   var dest = opts.dest || cwd;
   var data = opts.data || {};
 
+  app.engine('html', require('engine-handlebars'));
+
   app.data(data);
   app.option('production', isProduction);
   app.option('assets', assets);
   app.option('layout', layout);
-  app.engine('html', require('engine-handlebars'));
   app.layouts(layouts);
 
   if (_.isObject(partials)) {
@@ -80,46 +83,44 @@ function compileTemplates(options, done) {
  * @return {[type]}
  */
 function compileStyleguide(opts, done) {
-  var app = assemble.init();
   opts = opts || {};
 
+  var app = assemble.init();
   var cwd = process.cwd();
-  var isProduction = opts.production || config.production;
 
-  // the kickstart-assemble pages used to display the styleguide patterns
-  var src = opts.src ? path.resolve(cwd, opts.src) : config.src;
-  // this is the destination of the compiled styleguide
-  var dest = opts.dest ? path.resolve(cwd, opts.dest) : config.dest;
+  // defaults
+  var defaults = {
+    production: false,
+    assets: '',
+    helpers: path.resolve(__base, 'lib/helpers/helper-data.js'),
+    layouts: path.resolve(__base, 'lib/templates/layouts/*.hbs'),
+    layout: 'default-layout',
+    includes: path.resolve(__base, 'lib/templates/includes/**/*.hbs'),
+    src: path.resolve(__base, 'lib/templates/pages/**/*.hbs'),
+    dest: path.resolve(process.cwd(), 'styleguide')
+  };
 
-  // set the assets path if provided and make it relative to the dest just
-  // to be safe
-  var assets = opts.assetPath || '';
-  // we're only doing this in the case that the user is choosing to override
-  // the defaults for the styleguide generation
-  var helpers = opts.helpers || config.helpers;
-  // these really should be the styleguide layouts unless the user has
-  // passsed in their own layouts for the styleguide
-  var layout = opts.layout || config.layout;
-  var layouts = opts.layouts || config.layouts;
-  // these are the partials used by the styleguide
-  var includes = config.includes;
-  var patterns = opts.patterns || [];
-  // these are the user partials referenced in the user pages
-  var partials = opts.partials || opts.includes || opts.materials || null;
-  // these are the user pages that should be displayed within the styleguide
-  var pages = opts.pages ? path.resolve(cwd, opts.pages) : {};
-  // this is the user data that is referrenced with the user layouts, pages
-  // and partials
-  var data = opts.data ? opts.data : {};
+  // ensure paths are relative to running context
+  opts.src ? path.resolve(cwd, opts.src) : null;
+  opts.dest ? path.resolve(cwd, opts.dest) : null;
+  opts.pages ? path.resolve(cwd, opts.pages) : null;
+
+  // handle both assetsPath and assets
+  opts.assets = opts.assetPath || opts.assets;
+  // handle a range of property names for partials
+  opts.partials =  opts.partials || opts.materials || null;
+
+  // extend the default options with the user options
+  opts = _.extend(defaults, opts);
 
   // create assemble templates based on passed-in pattern categories
   // or just use the source files that were passed in if there are no patterns
   // passed in
-  if (_.isObject(partials)) {
-    var keys = Object.keys(partials);
+  if (_.isObject(opts.partials)) {
+    var keys = Object.keys(opts.partials);
 
-    if (patterns.length === 0) {
-      patterns = keys;
+    if (!opts.patterns || opts.patterns.length === 0) {
+      opts.patterns = keys;
     }
 
     keys.forEach(function(key) {
@@ -127,25 +128,25 @@ function compileStyleguide(opts, done) {
       var plural = pluralize(key);
 
       app.create(plural, singular, { renderType: 'partial' });
-      app[plural](partials[key]);
+      app[plural](opts.partials[key]);
     });
   } else {
-    app.partials(partials);
+    app.partials(opts.partials);
   }
-
-  app.data(data);
-  app.option('production', isProduction);
-  app.option('assets', assets);
-  app.option('layout', layout);
-  app.option('patterns', patterns);
 
   // register engine for .html files
   app.engine('html', require('engine-handlebars'));
 
+  app.data(opts.data);
+  app.option('production', opts.production);
+  app.option('assets', opts.assets);
+  app.option('layout', opts.layout);
+  app.option('patterns', opts.patterns);
+
   // register layouts and partials
-  app.layouts(layouts);
-  app.partials(includes);
-  app.pages(pages);
+  app.layouts(opts.layouts);
+  app.partials(opts.includes);
+  app.pages(opts.pages);
 
   app.asyncHelper('styleguide-collection', require('lib/helpers/helper-render-collection.js'));
   app.asyncHelper('styleguide-collections', require('lib/helpers/helper-render-collections.js'));
@@ -153,18 +154,17 @@ function compileStyleguide(opts, done) {
 
   // assemble task
   app.task('compile:styleguide', function() {
-    // console.log('app: ', app);
-    app.src(src)
+    app.src(opts.src)
       // .pipe(tap(function(file, t) {
       //   console.log('file name: ', file.path);
       // }))
       .pipe(extname())
-      .pipe(app.dest(dest));
+      .pipe(app.dest(opts.dest));
   });
 
   // copy assets task
   app.task('copy:assets', function() {
-    assemble.copy(__base + '/dist/assets/**', dest + '/public/');
+    assemble.copy(__base + '/dist/assets/**', opts.dest + '/public/');
   });
 
   // run the tasks, then execute the callback
