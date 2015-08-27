3f8263d41309f34178fd61a41ba80007121e4571
askesian <cwigley@gmail.com>
2015-08-27 14:28:12 -0500

    Updates for styleguide generation

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-// node modules
-var path = require('path');
-
-// node_modules modules
-var _ = require('lodash');
-var assemble = require('assemble');
-var defaults = require('object.defaults');
-var extname = require('gulp-extname');
-var tap = require('gulp-tap');
-var async = require('async');
+'use strict';
 
 // set __dirname as the base top simplify requiring local modules
 require('app-module-path').addPath(__dirname);
@@ -15,33 +6,38 @@ require('app-module-path').addPath(__dirname);
 // set a global variable to store root dir for path references
 global.__base = global.__base || __dirname + '/';
 
-// local modules
-var config = require('lib/config');
+// node modules
+var path = require('path');
 
-// export the functions for use in other modules
-module.exports.templates = compileTemplates;
-module.exports.styleguide = compileStyleguide;
+// node_modules modules
+var _ = require('lodash');
+var assemble = require('assemble');
+var async = require('async');
+var chalk = require('chalk');
+var defaults = require('object.defaults');
+var extname = require('gulp-extname');
+var glob = require('glob');
+var tap = require('gulp-tap');
 
 /**
- * [compileTemplates description]
+ * Compile Templates
  *
- * @param {[type]}   options [description]
- * @param {Function} done    [description]
+ * @param {object}   options
+ * @param {Function} done
  *
- * @return {[type]}
+ * @return {Function}
  */
-function compileTemplates(opts, done) {
+function compileSite(options, done) {
+
+  var opts = options || {};
   var app = assemble.init();
-  var cwd = process.cwd();
-
-  opts = opts || {};
-
+  var cwd = app.options.cwd || process.cwd();
   var isProduction = opts.production || false;
   var assets = opts.assets || '';
   var helpers = opts.helpers || [];
   var layout = opts.layout || null;
   var layouts = opts.layouts || [];
-  var partials = opts.partials || [];
+  var partials = opts.partials || opts.includes || [];
   var pages = opts.pages || [];
   var dest = opts.dest || cwd;
   var data = opts.data || {};
@@ -63,164 +59,129 @@ function compileTemplates(opts, done) {
     app.partials(partials);
   }
 
-  // assemble task
-  app.task('compile:templates', function() {
+  /**
+   * Compile Templates task
+   *
+   * @param {function}
+   */
+  app.task('compile:site', function() {
     return app.src(pages)
       .pipe(extname())
       .pipe(app.dest(dest));
   });
 
   // run the tasks, then execute the callback
-  app.run('compile:templates', done);
+  app.run('compile:site', done);
 }
 
 /**
- * [compileStyleguide description]
+ * Compile Styleguide
  *
- * @param {[type]}   opts [description]
- * @param {Function} done [description]
+ * @param {object}   opts
+ * @param {Function} done
  *
- * @return {[type]}
+ * @return {Function}
  */
-function compileStyleguide(opts, done) {
-  opts = opts || {};
-
+function compileStyleguide(options, done) {
+  var opts = options || {};
   var app = assemble.init();
-  var cwd = process.cwd();
+  var cwd = app.options.cwd || process.cwd();
 
-  // defaults
   var defaults = {
     production: false,
     assets: '',
-    helpers: path.resolve(__base, 'lib/helpers/helper-data.js'),
-    layouts: path.resolve(__base, 'lib/templates/layouts/*.hbs'),
+    helpers: path.join(__base, 'lib/helpers/helper-data.js'),
+    layouts: path.join(__base, 'lib/templates/layouts/*.{hbs,html}'),
     layout: 'default-layout',
-    includes: path.resolve(__base, 'lib/templates/includes/**/*.hbs'),
-    src: path.resolve(__base, 'lib/templates/pages/**/*.hbs'),
-    dest: path.resolve(process.cwd(), 'styleguide')
+    includes: path.join(__base, 'lib/templates/includes/**/*.{hbs,html}'),
+    src: path.join(__base, 'lib/templates/pages/**/*.{hbs,html}'),
+    dest: path.resolve(cwd, 'styleguide')
   };
-
-  // ensure paths are relative to running context
-  opts.src ? path.resolve(cwd, opts.src) : null;
-  opts.dest ? path.resolve(cwd, opts.dest) : null;
-  opts.pages ? path.resolve(cwd, opts.pages) : null;
 
   // handle both assetsPath and assets
   opts.assets = opts.assetPath || opts.assets;
   // handle a range of property names for partials
-  opts.partials =  opts.partials || opts.materials || null;
+  opts.patterns =  opts.patterns || opts.partials || opts.materials;
+  // set pattern group names if specified
+  opts.patternCategories = opts.patternCategories || [];
 
   // extend the default options with the user options
   opts = _.extend(defaults, opts);
 
-  // create assemble templates based on passed-in pattern categories
-  // or just use the source files that were passed in if there are no patterns
-  // passed in
-  if (_.isObject(opts.partials)) {
-    var keys = Object.keys(opts.partials);
-
-    if (!opts.patterns || opts.patterns.length === 0) {
-      opts.patterns = keys;
-    }
-
-    keys.forEach(function(key) {
-      var singular = singularize(key);
-      var plural = pluralize(key);
-
-      app.create(plural, singular, { renderType: 'partial' });
-      app[plural](opts.partials[key]);
-    });
-  } else {
-    app.partials(opts.partials);
-  }
-
+  // app.options['minimal config'] = true;
+  // ======================
+  // App Configuration
+  // ======================
   // register engine for .html files
   app.engine('html', require('engine-handlebars'));
-
+  // json,yml data sources
   app.data(opts.data);
   app.option('production', opts.production);
   app.option('assets', opts.assets);
   app.option('layout', opts.layout);
-  app.option('patterns', opts.patterns);
-
+   // register patterns
+  app.option('patternCategories', opts.patternCategories);
   // register layouts and partials
   app.layouts(opts.layouts);
   app.partials(opts.includes);
   app.pages(opts.pages);
 
+  // ======================
+  // Handle Pattern Files
+  // ======================
+  var collections = require('lib/loaders/pattern-collections.js')(app);
+
+  // load the pattern partials
+  // if the partials value is an object, the the keys are pattern categories
+  // so we iterate the keys and create custom partials for each category
+  // otherwise, we have a glob path to all partials and we need to group
+  // by category specified in each files front-matter
+  if (typeof opts.patterns === 'object') {
+    var keys = Object.keys(opts.patterns || {});
+    app.set('patternCategories', keys);
+    keys.forEach(function(key) {
+      collections.create(key, opts.patterns[key], { viewType: 'partial' });
+      //collections.addToPages(key);
+    });
+  } else {
+    var grouped = collections.groupBy(opts.patterns, 'category');
+    var keys = Object.keys(grouped || {});
+    app.set('patternCategories', keys);
+    keys.forEach(function(key) {
+      collections.create(key, grouped[key], { viewType: 'partial' });
+      //collections.writeFiles(key);
+    });
+  }
+
+  // ======================
+  // Helpers
+  // ======================
   app.asyncHelper('styleguide-collection', require('lib/helpers/helper-render-collection.js'));
   app.asyncHelper('styleguide-collections', require('lib/helpers/helper-render-collections.js'));
   app.asyncHelper('styleguide-navigation', require('lib/helpers/helper-render-navigation.js'));
 
-  // assemble task
+  /**
+   * Compile Styleguide task
+   *
+   * @param {function}
+   */
   app.task('compile:styleguide', function() {
-    app.src(opts.src)
-      // .pipe(tap(function(file, t) {
-      //   console.log('file name: ', file.path);
-      // }))
+    // copy static assets over to dest
+    app.copy(__base + '/dist/assets/**', opts.dest + '/public/');
+
+    // pipe the source files though to dest
+    return app.src(opts.src)
       .pipe(extname())
       .pipe(app.dest(opts.dest));
   });
 
-  // copy assets task
-  app.task('copy:assets', function() {
-    assemble.copy(__base + '/dist/assets/**', opts.dest + '/public/');
-  });
-
   // run the tasks, then execute the callback
-  return app.run(['compile:styleguide', 'copy:assets'], done);
+  return app.run('compile:styleguide', done);
 
 }
 
-/**
- * Detects if a string ends with a suffix
- *
- * @param {string} str    the string to check
- * @param {string} suffix
- *
- * @return {boolean} returns 'true' if string ends with the suffix, 'false' if not
- */
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
-
-/**
- * [isPlural description]
- *
- * @param {[type]} str [description]
- *
- * @return {Boolean}
- */
-function isPlural(str) {
-  return endsWith(str, 's');
-}
-
-/**
- * [pluralize description]
- *
- * @param {[type]} str [description]
- *
- * @return {[type]}
- */
-function pluralize(str) {
-  if (isPlural(str)) {
-    return str;
-  }
-
-  return str + 's';
-}
-
-/**
- * [singularize description]
- *
- * @param {[type]} str [description]
- *
- * @return {[type]}
- */
-function singularize(str) {
-  if (isPlural(str)) {
-    return str.substring(0, str.length - 1);
-  }
-
-  return str;
-}
+// export the functions for use in other modules
+module.exports = {
+  templates: compileSite,
+  styleguide: compileStyleguide
+};
